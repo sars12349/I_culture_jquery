484d0360b06a10c8ee542735880beef8005fae95
sars12349 <sars12349@gmail.com>
2019-01-26 11:41:09 +0800

    再一遍

---
+++ get_shop_info.php
@@ -0,0 +1,22 @@
+<?php
+	require_once("dbtools.inc.php");
+
+	$link=create_connection();
+    $kind=$_POST['kind'];
+	if($kind=='f'){
+		$sql="SELECT * FROM shop WHERE Kind='f' ORDER BY Shop_ID ASC";
+	}else if($kind=='c'){
+		$sql="SELECT * FROM shop WHERE Kind='c' ORDER BY Shop_ID ASC";
+	}	
+
+	$result = excute_sql($link, "I_culture", $sql);
+
+	$rows=mysqli_fetch_assoc($result);
+	$myArray=array();
+
+	do{
+		$myArray[]=$rows;
+	}while ($rows=mysqli_fetch_assoc($result)); 
+		echo json_encode($myArray);
+
+?>
